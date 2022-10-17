346ea83068c82cfc6d750b8edf167e5258e0fd53
YOUR_NAME <YOUR_EMAIL>
2022-10-17 18:52:57 +0700

    second_comit

--- Udemy/urls.py
+++ Udemy/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crm import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.first_page),
+    path('thanks/', views.thanks_page, name='thanks_page')
+
 ]
